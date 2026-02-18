9e5855d0e48e11e276e77676114db8fbc211db4c
YINNSTORE <146443900+YINNSTORE@users.noreply.github.com>
2026-02-18 12:09:53 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,3 +1,11 @@
+// middleware.js
+import { NextResponse } from "next/server";
+
+export function middleware() {
+  // IMPORTANT: middleware wajib ada biar Vercel gak 500
+  return NextResponse.next();
+}
+
 export const config = {
   matcher: ["/dashboard/:path*", "/order/:path*"], // JANGAN INCLUDE /topup
 };
